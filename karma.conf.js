1e48e9f78ed09edf1052c56d6807da61f394a684
Marcos Jesús Chávez Vega <m-j.chavez.v@hotmail.com>
2018-10-05 23:17:35 -0500

    Test for coveralls

--- karma.conf.js
+++ karma.conf.js
@@ -49,8 +49,9 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: [ 'progress', 'kjhtml', 'coverage-istanbul' ],
+    reporters: [ 'progress', 'kjhtml', 'coverage-istanbul', 'coveralls' ],
     coverageIstanbulReporter: {
+    reports: ['html', 'lcov', 'text-summary'],
      dir: './coverage',
      fixWebpackSourcePaths: true,
      reports: ['cobertura', 'lcov', 'text', 'text-summary'],
